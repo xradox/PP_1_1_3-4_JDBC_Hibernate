7be90068bfdeea7d74418c0be3dafe7bdcd60cbc
xxxradoxxx <trustus@inbox.ru>
2022-08-15 09:04:04 +0300

    fix UserServiceImpl with DAO interface

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,46 +1,38 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-//import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //private final UserDaoJDBCImpl daoJDBC;
-    private final UserDaoHibernateImpl daoHibernate;
+    private final UserDao userDao;
     public UserServiceImpl() {
-        this.daoHibernate = new UserDaoHibernateImpl();
-        //this.daoJDBC = new UserDaoJDBCImpl();
+        this.userDao = new UserDaoHibernateImpl();
     }
 
     public void createUsersTable() {
-        //daoJDBC.createUsersTable();
-        daoHibernate.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        //daoJDBC.dropUsersTable();
-        daoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        //daoJDBC.saveUser(name, lastName, age);
-        daoHibernate.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        //daoJDBC.removeUserById(id);
-        daoHibernate.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        //return daoJDBC.getAllUsers();
-        return daoHibernate.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        //daoJDBC.cleanUsersTable();
-        daoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
